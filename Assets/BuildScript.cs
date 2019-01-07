653bf3aa281ee654e87c3c975b8ca84f4d65c7c8
最初 <>
2019-01-07 15:15:09 +0900

    最初

---
+++ Assets/BuildScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class BuildScript
+{
+    // ビルド実行でAndroidのapkを作成する例
+    [UnityEditor.MenuItem("Tools/Build Project AllScene Android")]
+    public static void BuildProjectAllSceneAndroid()
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.Android);
+        List<string> allScene = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (scene.enabled)
+            {
+                allScene.Add(scene.path);
+            }
+        }
+//        PlayerSettings.bundleIdentifier = "com.yourcompany.newgame";
+//        PlayerSettings.statusBarHidden = true;
+        BuildPipeline.BuildPlayer(
+            allScene.ToArray(),
+            "newgame.apk",
+            BuildTarget.Android,
+            BuildOptions.None
+        );
+    }
+}
