437801829c5fa21e653d11562390ec66ffc3e44f
muramatsu <yasuo_muramatsu@visualize.co.jp>
2019-01-09 13:23:25 +0900

    かいしゅう

---
+++ Assets/Scripts/main.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class main : MonoBehaviour {
+	[SerializeField] Text text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = "" +Time.time;　		
+	}
+}
